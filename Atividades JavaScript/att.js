0edfe38d02199ca155f2c26b2e571e59044f5990
isac-barbosa <santosisac196@gmail.com>
2025-04-14 22:20:33 -0300

    Alguns testes que aprendi na aula

---
+++ Atividades JavaScript/att.js
@@ -0,0 +1,6 @@
+let ano = prompt("Para qual ano voce deseja viajar?")
+if(ano < 2020){
+    console.log("Viajando para o passado!")
+}if(ano >= 2020){
+    console.log("Rumo ao futuro")
+}
---
+++ Atividades JavaScript/script.js
@@ -0,0 +1,5 @@
+function TesteBotao(){
+    alert("Foi no lugar certo!")
+}function OutroTeste(){
+    alert("Mais uma vez, voce esta craque!!!")
+}
