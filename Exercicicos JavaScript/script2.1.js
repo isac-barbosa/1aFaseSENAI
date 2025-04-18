d6f33ab771019fee41b8cc33f59bad676850b38e
isac-barbosa <santosisac196@gmail.com>
2025-04-17 21:06:25 -0300

    Desafio feito em sala usando o button

---
+++ Exercicicos JavaScript/script2.1.js
@@ -0,0 +1,31 @@
+function somar(){
+    let num1, num2, resultado
+
+    num1 = Number(prompt("Primeiro numero"))
+    num2 = Number(prompt("Segundo numero"))
+
+    resultado = num1 + num2
+    alert("O resultado da soma é " + resultado)
+
+}function diminuir(){
+    
+    num1 = Number(prompt("Digite o primeiro numero"))
+    num2 = Number(prompt("Digite o segundo numero"))
+
+    resultado = num1 - num2
+    alert("O resultado da subtração é " + resultado)
+
+}function multiplicar(){
+    num1 = Number(prompt("Digite o primeiro numero"))
+    num2 = Number(prompt("Digite o segundo numero"))
+
+    resultado = num1 * num2
+    alert("O resultado da sua multiplicação é " + resultado)
+
+}function dividir(){
+    num1 = Number(prompt("Digite o primeiro numero"))
+    num2 = Number(prompt("Digite o segundo numero"))
+    
+    resultado = num1 / num2
+    alert("O resultado da sua divisão é " + resultado)
+}
