e071645931f01abfc809973ac5f81731ec91c5e4
isac-barbosa <santosisac196@gmail.com>
2025-04-17 22:13:32 -0300

    Terminar StarUber

---
+++ Exercicicos JavaScript/StarUber.js
@@ -0,0 +1,6 @@
+let distanciaEmKm = prompt("Qual a distancia em (KM) até o seu destino")
+let velocidadeDaLuz = 1000
+function calcularVelocidade(){
+    
+
+}
---
+++ Exercicicos JavaScript/desafioRafael.js
@@ -0,0 +1,12 @@
+let brinquedos = prompt("Quantos brinquedos você trouxe?")
+let moedas
+if(brinquedos == 0){
+    console.log("Opss sem brinquedos sem moedas :(")
+}
+if(brinquedos == 1){
+    console.log("Parabéns você ganhou uma moeda!")
+}if (brinquedos == 2){
+    console.log("Parabens você ganhou duas moedas!")
+}if (brinquedos >=3){
+    console.log("Parabens você ganhou três moedas")
+}
