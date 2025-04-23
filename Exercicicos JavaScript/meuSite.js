277df174312ab54a49893d669a11b41d330e6a2a
isac-barbosa <santosisac196@gmail.com>
2025-04-22 22:08:22 -0300

    Atividades no html prof Alex

---
+++ Exercicicos JavaScript/meuSite.js
@@ -0,0 +1,49 @@
+function exercicio1(){
+    let num1 = Number(prompt("Digite um numero"))
+    let num2 = Number(prompt("Digite outro numero"))
+    let soma = num1 + num2
+    alert(`A soma destes números é: ${soma}`)
+}
+function exercicio2(){
+    let num1 = Number(prompt("Digite um numero"))
+    let num2 = Number(prompt("Digite outro numero"))
+    let media = (num1 + num2) / 2
+    alert(`A media destes números é: ${media}`)
+}
+function exercicio1Lista(){
+    let nome = prompt("Digite seu nome:")
+    alert(`Olá ${nome} Seja muito bem-vindo(a)!` )
+}
+function exercicio2Lista(){
+    let num1 = Number(prompt("Digite um numero"))
+    let num2 = Number(prompt("Digite outro numero"))
+    let soma = num1 + num2
+    alert(`A soma destes números é: ${soma}`)
+}
+function exercicio3(){
+let farenheit
+let celsius = prompt("Digite uma temperatura em graus celsius")
+farenheit = (celsius * 9/5) + 32
+alert(`Esta temperatura é ${farenheit} graus farenheit`)
+}
+function exercicio4(){
+    let num = Number(prompt("Digite um numero"))
+    if(num % 2 == 0)[
+        alert("Número par")
+    ]
+    else{
+        alert("Número ímpar")
+    }
+}
+function exercicio5(){
+    let num1 = Number(prompt("Digite sua primeira nota"))
+    let num2 = Number(prompt("Digite sua segunda nota"))
+    let num3 = Number(prompt("Digite sua ultima nota"))
+    let media = (num1 + num2 + num3) / 3
+    alert(`A media desse aluno é: ${media}`)
+}
+function exercicio6(){
+    let peso = prompt("Digite seu peso")
+    let altura = prompt("Digite sua altura")
+    imc = peso / altura
+}
