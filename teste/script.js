1d51318507a3dc06db770bc7f07072b577387fbe
isac-barbosa <santosisac196@gmail.com>
2025-05-22 22:15:47 -0300

    Fiz um site na pasta teste

---
+++ teste/script.js
@@ -0,0 +1,9 @@
+function mostrarlogin(){
+    document.getElementById('login').style.display = 'flex'
+    document.getElementById('cadastro').style.display = 'none'
+
+}
+function mostrarCadastro(){
+    document.getElementById('login').style.display = 'none'
+    document.getElementById('cadastro').style.display = 'flex'
+}
