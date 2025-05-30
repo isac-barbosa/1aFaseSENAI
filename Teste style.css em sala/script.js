61d2c37f9af3df963e39871f95d141383f536ff6
isac-barbosa <santosisac196@gmail.com>
2025-05-29 22:15:34 -0300

    fiz alterações no testando style em sala

--- Teste style.css em sala/script.js
+++ Teste style.css em sala/script.js
@@ -1,9 +1,58 @@
+// let nome, email, senha
+// document.getElementById('inpLogNome')
+inicializar()
+let usuario = {
+    nome: 'isac',
+    email: '222@gmail.com',
+    senha: 'nuvem'
+}
+function cadastrar(){
+    usuario.nome = document.getElementById('inpCadNome').value
+    usuario.email = document.getElementById('inpCadEmail').value
+    usuario.senha = document.getElementById('inpCadSenha').value
+    alert("Cadastrado com sucesso")
+    console.log(usuario)
+    mostrarlogin()
+}
+function logar(){
+    let nome = document.getElementById('inpLogNome').value
+    let senha = document.getElementById('inpLogSenha').value
+    if(nome === usuario.nome && senha === usuario.senha){
+        alert("Login efetuado com sucesso")
+        mostrarProdutos()
+        limparInputs()
+    }else{
+        alert("Login não efetuado")
+    }
+}
 function mostrarlogin(){
+esconderTodas()
     document.getElementById('login').style.display = 'flex'
-    document.getElementById('cadastro').style.display = 'none'
-
+    document.getElementById('inpLogNome').focus()
 }
 function mostrarCadastro(){
-    document.getElementById('login').style.display = 'none'
+esconderTodas()
     document.getElementById('cadastro').style.display = 'flex'
+    document.getElementById('inpCadNome').focus()
+}
+function mostrarProdutos(){
+    esconderTodas()
+    document.getElementById('produtos').style.display = 'flex'
+}
+function esconderTodas(){
+document.getElementById('login').style.display = 'none'
+document.getElementById('cadastro').style.display = 'none'
+document.getElementById('produtos').style.display = 'none'
+//esconder novas pàginas
+}
+function limparInputs(){
+    document.getElementById('inpCadNome').value = '',
+    document.getElementById('inpCadEmail').value = '',
+    document.getElementById('inpCadSenha').value = '',
+    
+    document.getElementById('inpLogNome').value = '',
+    document.getElementById('inpLogSenha').value = ''
+}
+function inicializar(){
+    mostrarlogin()
 }
