2ebddabf2718325cac8fcf699f8266a1e7062f44
isac-barbosa <santosisac196@gmail.com>
2025-06-23 17:09:49 -0300

    Testando arrays

--- Exercicios usando arrays/desafioarrays.js
+++ Exercicios usando arrays/desafioarrays.js
@@ -2,3 +2,4 @@
 let numeros = [5, 12, 8, 130, 44, 7, 3, 19, 22];
 let numerosFiltrados = numeros.filter((numeros) => numeros.length > 10);
 console.log(numerosFiltrados);
+ 
---
+++ Exercicios usando arrays/exercicios guanabara/aula15.js
@@ -0,0 +1,5 @@
+let num = [5, 8, 2, 9,12]
+num[6] = 6 //Usado pra adicionar valor ao indice 5
+num.push(7) //Usado pra adicionar valor ao ultimo índice
+num.sort() //Pega todos os elementos e coloca em ordem crescente
+console.log(num[0])
